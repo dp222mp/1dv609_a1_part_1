1ef8bf0222cba77fedf1dc5f5b531cbb773334a2
dp222mp <dp222mp@student.lnu.se>
2025-12-26 22:42:57 -0800

    Gradle fixes + first two tests

--- java/practice/practice_with_run_tests/app/src/test/java/com/lab/PasswordTest.java
+++ java/practice/practice_with_run_tests/app/src/test/java/com/lab/PasswordTest.java
@@ -1,8 +1,8 @@
 package com.lab;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Test class for Password implementations.
@@ -24,20 +24,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordTest {
     private IPassword getPassword(String s) throws Exception {
-        return (IPassword) new Password(s);
-        // return (IPassword) new BugDoesNotTrim(s);
-        // return (IPassword) new BugToShortPassword(s);
-        // return (IPassword) new BugToShortPassword(s);
-        // return (IPassword) new BugVeryShort(s);
+        //return (IPassword) new Password(s);
+        //return (IPassword) new BugDoesNotTrim(s);
+        return (IPassword) new BugToShortPassword(s);
+        //return (IPassword) new BugVeryShort(s);
         // return (IPassword) new BugWrongExceptionMessage(s);
         // return (IPassword) new BugMissingPasswordLengthCheck(s);
         // return (IPassword) new BugMissingNumberCheck(s);
-        // return (IPassword) new BugIsPasswordSameAlwaysTrue(s);
-        // return (IPassword) new BugWrongHashingAlgorithm(s);
+        //return (IPassword) new BugIsPasswordSameAlwaysTrue(s);
+        //return (IPassword) new BugWrongHashingAlgorithm(s);
     }
 
     @Test
     public void shouldAlwaysPass() throws Exception {
         assertTrue(true);
     }
+
+    @Test
+    public void passwordsShouldIgnoreSurroundingWhitespace() throws Exception {
+        IPassword p1 = getPassword("ValidPassword1");
+        IPassword p2 = getPassword("   ValidPassword1   ");
+
+        assertTrue(
+            p1.isPasswordSame(p2),
+            "Whitepace around the password must be ignored for hashing"
+        );
+    }
+    @Test
+    public void shouldThrowForPasswordsShorterThan12() throws Exception {
+        Exception e1 = assertThrows(
+            Exception.class,
+            () -> getPassword("eleven11111"),
+            "Passwords shorter than 12 characters should throw an exception"
+        );
+    }
+
+
+
+
 }
